ed3cdba7507dbc1230bdc1541efa08e507361eab
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-04 19:03:28 +0100

    Update Weather_app.py

--- Weather_app.py
+++ Weather_app.py
@@ -4,14 +4,23 @@ import datetime as dt
 
 ## display date and time for a location
 def display_date_time_weather(city="Jerusalem"):
-    
-    user_time = dt.datetime.now(pytz.timezone(city))
+    user_time = dt.datetime.now(pytz.timezone(zone))
     formatted_user_time = user_time.strftime("%A, %B %d, %Y, %I:%M %p")
-    print(f"Your current date and time: {formatted_user_time}")
+    print(f"Your current date and time in {zone}: {formatted_user_time}")
+
+    #a=[]
+    #for zone in pytz.all_timezones:
+    #    zone_separated = zone.split("/")
+    #    city=zone_separated[len(zone_separated)-1]
+    #    a.append(city)
+
+    zone_separated = zone.split("/")
+    city=zone_separated[len(zone_separated)-1]  
+    print(f"The weather in {city}:")  
 
     API = 'e1313973fe262c3c18b4500d98fe65eb'
     url=f"https://api.openweathermap.org/data/2.5/weather?appid={API}&q={city}"
-    weatherzone = rq.get(url)
+    weatherzone = requests.get(url)
     response_weatherzone = weatherzone.json()
 
     humidity=response_weatherzone['main']['humidity']
@@ -25,5 +34,3 @@ def display_date_time_weather(city="Jerusalem"):
     print(f'Pressure:', {pressure})
     print(f'Humidity:', {humidity})
     print(f'Description:', {description})
-
-  
