c538395e92a04514996d2a8f5948e31d8d09e501
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-04 19:43:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import Weather_app as wa
 import streamlit as st
 import pytz
 import requests
@@ -7,4 +7,4 @@ import datetime as dt
 zone= st.selectbox("Choose a city", pytz.all_timezones)
 
 ## display date and time for a location
-st.write(Weather_app.display_date_time_weather(zone))
+st.write(wa.display_date_time_weather(zone))
