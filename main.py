e28a4950c1e3a4740588e4155dc66a5e58bd985b
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-04 19:11:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 
 import streamlit as st
 import pytz
+import requests
+import datetime as dt
+
 zone= st.selectbox("Choose a city", pytz.all_timezones)
+
+## display date and time for a location
+def display_date_time_weather(city="Jerusalem"):
