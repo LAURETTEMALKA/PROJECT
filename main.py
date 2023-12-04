1f291709ea07eab892360f623d6d1c29aa3bf425
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-04 19:24:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ import datetime as dt
 zone= st.selectbox("Choose a city", pytz.all_timezones)
 
 ## display date and time for a location
-display_date_time_weather(zone="Israel")
+date_time_weather=display_date_time_weather(zone)
+st.subheader(date_time_weather)
