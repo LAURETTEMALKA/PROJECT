9a1f7a6dfae4298b38e8c95d307203c530a64cbd
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-05 15:47:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,9 @@ def display_weather(city="Jerusalem", unit="metric"):
     wind=response_weatherzone['wind']['speed']
     description=response_weatherzone['weather'][0]['description']
     temp=response_weatherzone['main']['temp']
-    icon_code = response_weatherzone.get["weather"][0].get["icon"]
-    icon_url = f"https://openweathermap.org/img/wn/icon={icon_code}.png"
+    icon_code = response_weatherzone["weather"][0]["icon"]
+    icon = icon_url
+    icon_url = f"https://openweathermap.org/img/wn/icon={icon_}.png"
     #icon = icon_url
     
     a=f"The weather in {city} is:"
